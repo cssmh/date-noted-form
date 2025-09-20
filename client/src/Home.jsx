53b879e45747309f44a16a0e7a94637d0500628d
Momen <mominitmbstu@gmail.com>
2025-09-20 12:55:04 +0600

    reconnect git

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,12 +2,12 @@ import AllData from "./AllData";
 import App from "./App";
 
 const Home = () => {
-    return (
-        <div>
-            <App />
-            <AllData />
-        </div>
-    );
+  return (
+    <div>
+      <App />
+      <AllData />
+    </div>
+  );
 };
 
 export default Home;
